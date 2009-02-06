15d2a7ba009d743a3047675dc340c608586baef0
Matt Yoho <mbyoho@gmail.com>
2009-02-06 00:28:20 -0500

    tweaks

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
   def next_karaoke_night
-    link_to("Tonight at Crown Sports Lounge @ 9PM.", karaoke_path)
+    link_to("Monday night at Donericks Pub House @ 9PM.", karaoke_path)
   end
 end
