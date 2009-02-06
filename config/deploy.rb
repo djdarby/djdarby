ba7be272de3040e1bc6f74879a382c4e3dd521ce
Matt Yoho <mbyoho@gmail.com>
2009-02-06 15:46:49 -0500

    reverted some testing tweaks

---
+++ config/deploy.rb
@@ -0,0 +1,30 @@
+set :application, "djdarby"
+ 
+set :deploy_to, "/var/www/djdarby"
+ 
+set :scm, :git
+set :repository, "git@github.com:mattyoho/djdarby.git"
+set :branch, "master"
+set :deploy_via, :remote_cache
+ 
+set :user, 'mbyoho'
+set :ssh_options, { :forward_agent => true }
+ 
+
+role :app, "djdarby.com"
+role :web, "djdarby.com"
+role :db,  "djdarby.com", :primary => true
+ 
+namespace :deploy do
+  desc "Restarting mod_rails with restart.txt"
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    #run "touch #{current_path}/tmp/restart.txt"
+    run "touch #{deploy_to}/current/tmp/restart.txt"
+  end
+ 
+  [:start, :stop].each do |t|
+    desc "#{t} task is a no-op with mod_rails"
+    task t, :roles => :app do ; end
+  end
+end
+
